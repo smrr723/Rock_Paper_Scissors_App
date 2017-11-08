9318e42ccbec80519386a0b13c7bef270198b88c
Scott Murray <rai.five@gmail.com>
2017-11-08 14:45:44 +0000

    Initial Commit

---
+++ app/src/main/java/com/example/scott/rockpaperscissors/RockPaperScissorActivity.java
@@ -0,0 +1,110 @@
+package com.example.scott.rockpaperscissors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class RockPaperScissorActivity extends AppCompatActivity {
+
+    private ArrayList<String> choices;
+    private TextView opponentMove;
+    private TextView resultText;
+    private Button rockButton;
+    private Button paperButton;
+    private Button scissorsButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rock_paper_scissor);
+        opponentMove = (TextView)findViewById(R.id.opponent_move);
+        resultText = (TextView)findViewById(R.id.result_text);
+        rockButton = (Button)findViewById(R.id.rock_button);
+        paperButton = (Button)findViewById(R.id.paper_button);
+        scissorsButton = (Button)findViewById(R.id.scissors_button);
+        this.choices = new ArrayList<>();
+    }
+
+    public ArrayList<String> getChoices(){
+        choices.add(0, "Rock");
+        choices.add(1, "Paper");
+        choices.add(2, "Scissors");
+        return choices;
+    }
+
+    public String randChoice(){
+        Random rand = new Random();
+        int choiceSize = getChoices().size();
+        int random = rand.nextInt(choiceSize);
+        return getChoices().get(random);
+    }
+
+    public String play(String choice) {
+        String opponentResult = randChoice();
+        if(opponentResult == choice){
+            return "Draw!";
+        }
+        else if(opponentResult == "Rock" && choice == "Paper"){
+            resultText.setText("You win!");
+            opponentMove.setText("Rock");
+            return "You win!";
+        }
+        else if(opponentResult == "Rock" && choice == "Scissors"){
+            resultText.setText("You lose!");
+            opponentMove.setText("Rock");
+            return "You lose!";
+        }
+        else if(opponentResult == "Paper" && choice == "Scissors"){
+            resultText.setText("You win!");
+            opponentMove.setText("Paper");
+            return "You win!";
+        }
+        else if(opponentResult == "Paper" && choice == "Rock"){
+            resultText.setText("You lose!");
+            opponentMove.setText("Paper");
+            return "You lose!";
+        }
+        else if(opponentResult == "Scissors" && choice == "Paper"){
+            resultText.setText("You lose!");
+            opponentMove.setText("Scissors");
+            return "You lose!";
+        }
+        else if(opponentResult == "Scissors" && choice == "Rock"){
+            resultText.setText("You win!");
+            opponentMove.setText("Scissors");
+            return "You win!";
+        }
+
+        return "Play again?";
+
+    }
+
+    public void onPaperButtonClicked(View button) {
+        Log.d(getClass().toString(), "onPaperButtonClicked was called");
+        play("Paper");
+//        Thread.sleep(4000);
+//        resultText.setText("--");
+//        opponentMove.setText("--");
+    }
+    public void onRockButtonClicked(View button) {
+        Log.d(getClass().toString(), "onRockButtonClicked was called");
+        play("Rock");
+
+    }
+    public void onScissorsButtonClicked(View button) {
+        Log.d(getClass().toString(), "onScissorsButtonClicked was called");
+        play("Scissors");
+
+    }
+
+}
